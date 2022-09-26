477ec57a191d7d818e5ef40d3282b9ed6c038c8e
Federico Stra <stra.federico@gmail.com>
2022-09-26 17:10:08 +0200

    Add integration test

---
+++ tests/src_01.rs
@@ -0,0 +1,13 @@
+use assert2::*;
+
+use texfmt::lexer::lex_tokens;
+
+#[test]
+fn tokenize_all() {
+    let src = include_str!("../assets/src_01.tex");
+    let result = lex_tokens(src);
+    let_assert!(Ok((rest, tokens)) = result);
+    check!(rest.is_empty());
+    check!(!tokens.is_empty());
+    check!(tokens.len() == 287);
+}
